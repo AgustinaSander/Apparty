a5fea38bf7639fce3dad4facab083754021bf978
Agustina Sander <asander00@hotmail.com>
2023-01-05 21:01:07 -0300

    Add menu

--- app/src/main/java/com/example/apparty/MainActivity.java
+++ app/src/main/java/com/example/apparty/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.apparty;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.apparty.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        setContentView(view);
+        setSupportActionBar(binding.materialToolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+   /* @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+    }*/
 }
