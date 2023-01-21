21ffd79ffc6c636661d71a889d310767456373c0
Agustina Sander <asander00@hotmail.com>
2023-01-21 01:56:36 -0300

    Tabs and navigation

--- app/src/main/java/com/example/apparty/PurchaseFragment.java
+++ app/src/main/java/com/example/apparty/PurchaseFragment.java
@@ -2,16 +2,21 @@ package com.example.apparty;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.apparty.databinding.FragmentPurchaseBinding;
+import com.google.android.material.tabs.TabLayout;
 
 public class PurchaseFragment extends Fragment {
+
+    TabLayout tabLayout;
+    ViewPager2 viewPager;
+    ViewPagerAdapter viewPagerAdapter;
 
     private FragmentPurchaseBinding binding;
 
@@ -20,7 +25,6 @@ public class PurchaseFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
     }
 
@@ -28,6 +32,27 @@ public class PurchaseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentPurchaseBinding.inflate(inflater, container, false);
+
+        viewPager = binding.pager;
+        tabLayout = binding.tablayout;
+        viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(viewPagerAdapter);
+        viewPager.setUserInputEnabled(false);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
         return binding.getRoot();
     }
 }
