9efa222420746a6a196fbaf900f8cbbf473c10bf
Agustina Sander <asander00@hotmail.com>
2023-02-13 15:58:46 -0300

    Trying to generate qr

--- app/src/main/java/com/example/apparty/CompletePurchaseDialogFragment.java
+++ app/src/main/java/com/example/apparty/CompletePurchaseDialogFragment.java
@@ -1,16 +1,26 @@
 package com.example.apparty;
 
+import static android.content.Context.WINDOW_SERVICE;
+
+import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.example.apparty.databinding.FragmentCompletePurchaseDialogBinding;
 import com.google.android.material.snackbar.Snackbar;
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+
 
 public class CompletePurchaseDialogFragment extends DialogFragment {
 
@@ -32,7 +42,23 @@ public class CompletePurchaseDialogFragment extends DialogFragment {
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        generateQR();
         setClickEvents();
+    }
+
+    private void generateQR() {
+        WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        Display display = manager.getDefaultDisplay();
+        Point point = new Point();
+        display.getSize(point);
+        int width = point.x;
+        int height = point.y;
+        int smallerDimension = width < height ? width : height;
+        smallerDimension = smallerDimension * 3 / 4;
+        QRGEncoder qrgEncoder = new QRGEncoder("hola", null, QRGContents.Type.TEXT, 2000);
+        Bitmap myBitmap = qrgEncoder.getBitmap();
+        ImageView qrImage = binding.qrImage;
+        qrImage.setImageBitmap(myBitmap);
     }
 
     private void setClickEvents() {
