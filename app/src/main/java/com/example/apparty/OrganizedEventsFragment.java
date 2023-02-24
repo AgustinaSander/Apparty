48128294d093fb9f145951a35994f6c1457a0ea4
Agustina Sander <asander00@hotmail.com>
2023-02-23 22:33:36 -0300

    Disable selection from past events

--- app/src/main/java/com/example/apparty/OrganizedEventsFragment.java
+++ app/src/main/java/com/example/apparty/OrganizedEventsFragment.java
@@ -18,6 +18,7 @@ import com.example.apparty.databinding.FragmentOrganizedEventsBinding;
 import com.example.apparty.gestores.GestorEvent;
 import com.example.apparty.model.Event;
 import com.example.apparty.model.User;
+import com.google.android.material.tabs.TabLayout;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -29,14 +30,15 @@ import lombok.NonNull;
 public class OrganizedEventsFragment extends Fragment implements ResultsRecyclerAdapter.OnNoteListener{
 
     private FragmentOrganizedEventsBinding binding;
-    private RecyclerView recyclerView;
-    private RecyclerView recyclerViewToday;
-    private RecyclerView.Adapter adapter;
-    private RecyclerView.Adapter adapterToday;
-    private RecyclerView.LayoutManager layoutManager;
-    private RecyclerView.LayoutManager layoutManagerToday;
-    private List<Event> myEvents = new ArrayList<>();
-    private List<Event> todayEvents = new ArrayList<>();
+    private RecyclerView recyclerViewNext;
+    private RecyclerView recyclerViewPast;
+    private RecyclerView.Adapter adapterNext;
+    private RecyclerView.Adapter adapterPast;
+    private RecyclerView.LayoutManager layoutManagerNext;
+    private RecyclerView.LayoutManager layoutManagerPast;
+    private TabLayout tabLayout;
+    private List<Event> nextEvents = new ArrayList<>();
+    private List<Event> pastEvents = new ArrayList<>();
     private GestorEvent gestorEvent;
 
     public OrganizedEventsFragment() {}
@@ -50,48 +52,66 @@ public class OrganizedEventsFragment extends Fragment implements ResultsRecycler
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentOrganizedEventsBinding.inflate(inflater,container,false);
+        tabLayout = binding.tabLayout;
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-
         super.onViewCreated(view, savedInstanceState);
         gestorEvent = GestorEvent.getInstance(getContext());
-        recyclerView = binding.recyclerOrganizedEvents;
-        recyclerViewToday = binding.recyclerTodayOrganizedEvents;
-        recyclerView.setHasFixedSize(true);
-        recyclerViewToday.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(view.getContext());
-        layoutManagerToday = new LinearLayoutManager(view.getContext());
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerViewToday.setLayoutManager(layoutManagerToday);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tabLayout.getSelectedTabPosition();
+                if(position==0){
+                    binding.nextEventsLayout.setVisibility(View.VISIBLE);
+                    binding.pastEventsLayout.setVisibility(View.GONE);
+                } else {
+                    binding.nextEventsLayout.setVisibility(View.GONE);
+                    binding.pastEventsLayout.setVisibility(View.VISIBLE);
+                }
+            }
+            @Override public void onTabUnselected(TabLayout.Tab tab) {}
+            @Override public void onTabReselected(TabLayout.Tab tab) {}
+        });
+
+        recyclerViewNext = binding.recyclerNextEvents;
+        recyclerViewPast = binding.recyclerPastEvents;
+        recyclerViewNext.setHasFixedSize(true);
+        recyclerViewPast.setHasFixedSize(true);
+        layoutManagerNext = new LinearLayoutManager(view.getContext());
+        layoutManagerPast = new LinearLayoutManager(view.getContext());
+        recyclerViewNext.setLayoutManager(layoutManagerNext);
+        recyclerViewPast.setLayoutManager(layoutManagerPast);
 
         setEventList();
 
-        if(myEvents.size()==0){
-            binding.noResults.setVisibility(View.VISIBLE);
+        if(nextEvents.size()==0){
+            binding.noResultsNextEvents.setVisibility(View.VISIBLE);
             binding.textResume.setVisibility(View.GONE);
-            binding.noResultsToday.setVisibility(View.GONE);
         } else {
-            binding.noResults.setVisibility(View.GONE);
+            binding.noResultsNextEvents.setVisibility(View.GONE);
             binding.textResume.setVisibility(View.VISIBLE);
         }
 
-        if(todayEvents.size()==0){
-            binding.noResultsToday.setVisibility(View.VISIBLE);
+        if(pastEvents.size()==0){
+            binding.noResultsPastEvents.setVisibility(View.VISIBLE);
+            binding.textResume.setVisibility(View.GONE);
         } else {
-            binding.noResultsToday.setVisibility(View.GONE);
+            binding.noResultsPastEvents.setVisibility(View.GONE);
+            binding.textResume.setVisibility(View.VISIBLE);
         }
 
-        adapter = new ResultsRecyclerAdapter(myEvents, this);
-        adapterToday = new ResultsRecyclerAdapter(todayEvents, this);
-        recyclerView.setAdapter(adapter);
-        recyclerViewToday.setAdapter(adapterToday);
+        adapterNext = new ResultsRecyclerAdapter(nextEvents, this);
+        adapterPast = new ResultsRecyclerAdapter(pastEvents, this);
 
-        //DESHABILITAR CLICK DE LOS ITEMS FUTUROS
-        recyclerView.setClickable(true);
-        recyclerViewToday.setClickable(true);
+        recyclerViewNext.setAdapter(adapterNext);
+        recyclerViewPast.setAdapter(adapterPast);
+
+        recyclerViewNext.setClickable(true);
+        recyclerViewPast.setClickable(false);
 
         binding.createEventBtn.setOnClickListener(e -> {
             NavHostFragment.findNavController(OrganizedEventsFragment.this).navigate(R.id.action_organizedEventsFragment_to_fragmentRegisterEvent);
@@ -102,14 +122,21 @@ public class OrganizedEventsFragment extends Fragment implements ResultsRecycler
         SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences("loginInfo",MODE_PRIVATE);
         int idUser = sharedPreferences.getInt("idUser", 0);
 
-        myEvents = gestorEvent.getEventsOrganizedByUser(idUser);
-        todayEvents = myEvents.stream().filter(e -> e.getDate().isEqual(LocalDate.now())).collect(Collectors.toList());
+        List<Event> myEvents = gestorEvent.getEventsOrganizedByUser(idUser);
+        nextEvents = myEvents.stream()
+                .filter(e -> e.getDate().isEqual(LocalDate.now()) || e.getDate().isAfter(LocalDate.now()))
+                .collect(Collectors.toList());
+        pastEvents = myEvents.stream()
+                .filter(e -> e.getDate().isBefore(LocalDate.now()))
+                .collect(Collectors.toList());
     }
 
     @Override
     public void onNoteClick(int idEvent) {
-        Bundle bundle = new Bundle();
-        bundle.putInt("idEvent", idEvent);
-        NavHostFragment.findNavController(OrganizedEventsFragment.this).navigate(R.id.action_organizedEventsFragment_to_scanQRFragment, bundle);
+        if(binding.pastEventsLayout.getVisibility() == View.GONE){
+            Bundle bundle = new Bundle();
+            bundle.putInt("idEvent", idEvent);
+            NavHostFragment.findNavController(OrganizedEventsFragment.this).navigate(R.id.action_organizedEventsFragment_to_scanQRFragment, bundle);
+        }
     }
 }
